b3c524e128c57f33477a6a3fad8fd90e464a4ae0
W. Brian Gourlie <bgourlie@gmail.com>
2016-06-05 17:19:14 -0500

    Initial commit

---
+++ BucketBot/BucketDiscordClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using Discord;
+
+namespace BucketBot
+{
+    /// <summary>
+    ///     Wrap the DiscordClient and provide IObservables instead of events
+    /// </summary>
+    public class BucketDiscordClient
+    {
+        private readonly DiscordClient _wrapped;
+        public readonly IObservable<Unit> OnConnect;
+        public readonly IObservable<MessageEventArgs> OnMessage;
+        public readonly IObservable<object> OnReady;
+
+        public BucketDiscordClient(string botToken)
+        {
+            _wrapped = new DiscordClient();
+            OnConnect = _wrapped.Connect(botToken).ToObservable();
+            OnReady = Observable.FromEventPattern(h => _wrapped.Ready += h, h => _wrapped.Ready -= h);
+            OnMessage = Observable.FromEventPattern<MessageEventArgs>(h => _wrapped.MessageReceived += h,
+                h => _wrapped.MessageReceived -= h).Select(o => o.EventArgs);
+        }
+    }
+}
---
+++ BucketBot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BucketBot
+{
+    internal class Program
+    {
+        private static readonly CancellationTokenSource Wtoken = new CancellationTokenSource();
+        private static int _messageCount;
+
+        private static void Main()
+        {
+            Run().Wait();
+        }
+
+        private static async Task Run()
+        {
+            var client = new BucketDiscordClient("MTg5MDg5NzEyNTEyODI3Mzkz.CjYIrA.SO48yZZZgUVAuCbK07EV-MJT7Fc");
+
+            client.OnMessage.Where(m => !m.User.IsBot)
+                .Subscribe(async e =>
+                {
+                    Interlocked.Increment(ref _messageCount);
+                    if (_messageCount%5 == 0)
+                    {
+                        await e.Channel.SendMessage($"{e.User.Mention} You're dumb");
+                    }
+                });
+
+            client.OnReady.Subscribe(async e => { await Console.Out.WriteLineAsync("ready"); });
+            client.OnConnect.Subscribe(async e => { await Console.Out.WriteLineAsync("Connected"); });
+
+            while (true)
+            {
+                await Task.Delay(10000, Wtoken.Token);
+            }
+        }
+    }
+}
